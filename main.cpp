24be5ec64fb6aa72020aa9e486dd1f1ff3a56481
ShadowPlayer34 <61494312+ShadowPlayer34@users.noreply.github.com>
2022-04-06 22:21:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,7 @@ int main() {
     double midday = 0;
     srand(time(NULL));
     //for (int o = 0; o < 100; o++) {
-        for (int i = 0;; i++) {
-            if (count >= 100) {
-                cout << "Всего выполнено: " << count << "%" << endl;
-                day = i;
-                break;
-            }
+        for (int i = 1;; i++) {
             for (int y = 0; y < 3; y++){
                 high[y] = (double)((0.2*pow(10,6) + rand()%(int)(0.5*pow(10,6)-0.2*pow(10,6)))/pow(10,6));
                 disp = (double)((5.4*pow(10,6) + rand()%(int)(6.6*pow(10,6)-5.4*pow(10,6)))/pow(10,6));
@@ -26,7 +21,7 @@ int main() {
             }
             for (int y = 0; y < 15; y++){
                 middle[y] = (double)((0.1*pow(10,6) + rand()%(int)(0.3*pow(10,6)-0.1*pow(10,6)))/pow(10,6));
-                disp = (double)((5.5*pow(10,6) + rand()%(int)(6.5*pow(10,6)-5.5*pow(10,6)))/pow(10,6));
+                disp = (double)((4.5*pow(10,6) + rand()%(int)(5.5*pow(10,6)-4.5*pow(10,6)))/pow(10,6));
                 count += middle[y]*disp;
             }
             for (int y = 0; y < 5; y++){
@@ -40,14 +35,15 @@ int main() {
                 disp = (double)((1.8*pow(10,6) + rand()%(int)(2.2*pow(10,6)-1.8*pow(10,6)))/pow(10,6));
                 count += noob[y]*disp;
             }
-            
+            if (count >= 100) {
+                cout << "Всего выполнено: " << count << "%" << endl;
+                day = i;
+                break;
+            }
             
         }
         cout << "Среднее значение: " << count/day << "%" << endl;
         cout << "За сколько дней выполнено: " << day << endl;
-        if (day == 5) {
-            cout << "УВОЛИТЬ!!!!!\n";
-        }
         midday += day;
         count = 0;
         day = 0;
